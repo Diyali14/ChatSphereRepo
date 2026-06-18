3a39d410aa63906474e3e678cc32388f70cf02b6
D14 <134128246+Diyali14@users.noreply.github.com>
2026-06-18 07:29:10 +0530

    Add import for stream collectors in GroupService

--- group-service/src/main/java/com/chatsphere/group/service/GroupService.java
+++ group-service/src/main/java/com/chatsphere/group/service/GroupService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import java.util.stream.Collectors;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.StringRedisTemplate;
